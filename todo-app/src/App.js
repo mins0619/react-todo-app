c342dbd17ef1fba9c6c244cf6af39b2b5fafc443
mins0619 <mins5588@naver.com>
2021-05-15 08:57:25 +0900

    프로젝트 생성커밋

---
+++ todo-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+
+const App = () => {
+  return <TodoTemplate>
+            <TodoInsert />
+         </TodoTemplate>
+};
+
+export default App;
